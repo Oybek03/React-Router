8bd1c428ddb7ec91e559784a8d1a6d2beaa7a508
Oybek <oybekjonxayrullayevich@gmail.com>
2022-05-20 09:46:34 +0500

    react-router-dom starter

---
+++ src/Blog.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Blog = () => {
+  return (
+    <div>
+      <h1>Blog</h1>
+      <p>this is my Blog</p>
+    </div>
+  );
+};
+export default Blog;
---
+++ src/Nav.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div className="nav">
+      <a href="/blog">Blog</a>
+      <a href="/blog">Contact</a>
+    </div>
+  );
+};
+
+export default Nav;
